8c54b383b0b19af3e47a47f67d047343d3034c34
KDang <22520187@gm.uit.edu.vn>
2025-04-23 16:35:26 +0700

    config app.jsx

---
+++ fintrack.client/src/components/User/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+      <nav>
+        {/* Add your navigation elements here */}
+      </nav>
+    );
+  }
+  
+  export default Navbar;
---
+++ fintrack.client/src/pages/User/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">Welcome to FinTrack</h1>
+        <p>Your personal finance tracking solution</p>
+      </div>
+    );
+  }
+  
+  export default Home;
